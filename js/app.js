50b24802cf84d5f5d6b4543baa1803357c58c144
ArchibaldLeMagnifique <denis.lachartre@free.fr>
2017-10-18 13:48:59 +0200

    le carousel est anguleux

--- js/app.js
+++ js/app.js
@@ -1,13 +1,34 @@
-var app = angular.module('app', ['ngRoute'])
+angular.module('app', ['ngAnimate', 'ngSanitize', 'ngRoute', 'ui.bootstrap']);
+angular.module('app').controller('CarouselDemoCtrl', function ($scope) {
+    $scope.myInterval = 5000;
+    $scope.noWrapSlides = false;
+    $scope.active = 0;
+    var slides = $scope.slides = [];
+    var currIndex = 0;
+
+    $scope.addSlide = function() {
+        slides.push({
+            image: 'test/spectacle1.jpg',
+            masque: 'img/masque.png',
+            id: currIndex++
+        });
+    };
+
+    for (var i = 0; i < 3; i++) {
+        $scope.addSlide();
+    }
+
+});
+
+
+
+angular.module('app')
     .config(function($routeProvider) {
         $routeProvider
-
-        // route for the home page
             .when('/', {
                 templateUrl : 'accueil.html'
             })
 
-            // route for the about page
             .when('/recherche', {
                 templateUrl : 'recherche.html'
             })
