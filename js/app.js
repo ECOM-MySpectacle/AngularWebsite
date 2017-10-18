3b8f9e23391f18a1017158ed89a8c16ae18883d8
ArchibaldLeMagnifique <denis.lachartre@free.fr>
2017-10-18 16:02:43 +0200

    carousel, bouton recherche et changement de page : OK

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-angular.module('app', ['ngAnimate', 'ngSanitize', 'ngRoute', 'ui.bootstrap']);
+angular.module('app', ['ngAnimate', 'ngSanitize', 'ngRoute', 'restangular', 'ui.bootstrap']);
 angular.module('app').controller('CarouselDemoCtrl', function ($scope) {
     $scope.myInterval = 5000;
     $scope.noWrapSlides = false;
@@ -6,21 +6,32 @@ angular.module('app').controller('CarouselDemoCtrl', function ($scope) {
     var slides = $scope.slides = [];
     var currIndex = 0;
 
-    $scope.addSlide = function() {
+    $scope.addSlide = function(i) {
         slides.push({
-            image: 'test/spectacle1.jpg',
+            image: 'test/spectacle'+i+'.jpg',
             masque: 'img/masque.png',
+            info: 'test/spectacle'+i+'info.html',
             id: currIndex++
         });
     };
 
-    for (var i = 0; i < 3; i++) {
-        $scope.addSlide();
+    for (var i = 1; i <= 3; i++) {
+        $scope.addSlide(i);
     }
-
 });
 
 
+angular.module('app').controller('rechercher', function ($scope, Restangular) {
+    $scope.rechercher = function () {
+        var query = document.getElementById("query").value;
+
+        Restangular.all('students');
+
+        location.href='#!recherche';
+    };
+
+
+});
 
 angular.module('app')
     .config(function($routeProvider) {
@@ -31,5 +42,9 @@ angular.module('app')
 
             .when('/recherche', {
                 templateUrl : 'recherche.html'
+            })
+
+            .when('/billets', {
+                templateUrl : 'billets.html'
             })
     });
