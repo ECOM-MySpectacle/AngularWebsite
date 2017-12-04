8a45d79cd645ef2683c5780f9d3166ea5c82e670
ArchibaldLeMagnifique <denis.lachartre@free.fr>
2017-12-04 22:41:25 +0100

    plus de carourou

--- js/app.js
+++ js/app.js
@@ -19,28 +19,6 @@ app.service('modalService', function($uibModal,$uibModalStack){
     return modalService;
 });
 
-
-app.controller('CarouselDemoCtrl', function ($scope) {
-    $scope.myInterval = 5000;
-    $scope.noWrapSlides = false;
-    $scope.active = 0;
-    var slides = $scope.slides = [];
-    var currIndex = 0;
-
-    $scope.addSlide = function(i) {
-        slides.push({
-            image: 'test/spectacle'+i+'.jpg',
-            masque: 'img/masque.png',
-            info: 'test/spectacle'+i+'info.html',
-            id: currIndex++
-        });
-    };
-
-    for (var i = 1; i <= 3; i++) {
-        $scope.addSlide(i);
-    }
-});
-
 app.controller("connexionController", function($scope,$uibModalInstance,modalService){
     $scope.openModal = function(url, controller) {
         modalService.openModal(url,controller);
